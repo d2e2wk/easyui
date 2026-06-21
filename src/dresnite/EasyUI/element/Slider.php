2356a1c4ae0b9070aa281bb730a8ac25228755a8
d2e2wk <158263450+d2e2wk@users.noreply.github.com>
2026-06-21 16:18:09 +0600

    Update Slider.php

--- src/dresnite/EasyUI/element/Slider.php
+++ src/dresnite/EasyUI/element/Slider.php
@@ -5,23 +5,14 @@
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
  */
-
 declare(strict_types=1);
-
-
 namespace dresnite\EasyUI\element;
-
-
 class Slider extends Element {
-
     private float $minStep;
     private float $maxStep;
-
     private float $defaultStep;
     private float $stepLength;
-
     private ?float $submittedStep = null;
-
     public function __construct(?string $headerText, float $minStep, float $maxStep, float $defaultStep = 0, float $stepLength = 0) {
         $this->minStep = $minStep;
         $this->maxStep = $maxStep;
@@ -29,55 +20,42 @@ class Slider extends Element {
         $this->stepLength = $stepLength;
         parent::__construct($headerText);
     }
-
     public function getSubmittedStep(): ?float {
         return $this->submittedStep;
     }
-
     public function isReadOnly(): bool {
         return false;
     }
-
     public function getType(): string {
         return Element::TYPE_SLIDER;
     }
-
     public function getMinStep(): float {
         return $this->minStep;
     }
-
     public function setMinStep(float $minStep): void {
         $this->minStep = $minStep;
     }
-
     public function getMaxStep(): float {
         return $this->maxStep;
     }
-
     public function setMaxStep(float $maxStep): void {
         $this->maxStep = $maxStep;
     }
-
     public function getDefaultStep(): float {
         return $this->defaultStep;
     }
-
     public function setDefaultStep(float $defaultStep): void {
         $this->defaultStep = $defaultStep;
     }
-
     public function getStepLength(): float {
         return $this->stepLength;
     }
-
     public function setStepLength(float $stepLength): void {
         $this->stepLength = $stepLength;
     }
-
     public function assignResult($result): void {
-        $this->submittedStep = $result;
+        $this->submittedStep = is_bool($result) ? null : (float) $result;
     }
-
     public function serializeBody(): array {
         return [
             "min" => $this->minStep,
@@ -86,5 +64,4 @@ class Slider extends Element {
             "step" => $this->stepLength
         ];
     }
-
 }
